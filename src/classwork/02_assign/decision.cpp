d8e70e57130f0512e07c32da807c4a76f0878a75
Ching Lee <ching.lee@g.austincc.edu>
2020-02-06 19:26:31 -0600

    switch statement, assignment 2 complete

--- src/classwork/02_assign/decision.cpp
+++ src/classwork/02_assign/decision.cpp
@@ -1,1 +1,18 @@
 //Write the function code that returns the product of hours and hourly_rate.
+#include "decision.h"
+
+double gross_pay(double hours, double hourly_rate) {
+	double pay;
+
+	if (hours < 40) {
+		pay = hours * hourly_rate;
+
+		return pay;
+	}
+
+		else if (hours > 40) {
+			pay = (hours * hourly_rate) + ((hours - 40) * (hourly_rate * 1.5));
+
+		return pay;
+	}
+}
--- src/classwork/02_assign/main.cpp
+++ src/classwork/02_assign/main.cpp
@@ -1,23 +1,36 @@
 //Write the include statement for decisions.h here
+#include<iostream>
+#include "decision.h"
 
 //Write include for capturing input from keyboard and displaying output to screen
+using std::cout;
+using std::cin;
 
 
 //Write namespace using statements for cout and cin
 
-int main() 
-{
+int main() {
 	//create a double variable named hours
 	//create a double variable named hourly_rate
+	double hours;
+	double hourly_rate;
 
 	//Prompt user for keyboard input for hours and hourly_rate
 	//save the input to variables hours and hourly_rate
+	cout << "Total hours worked: ";
+	cin >> hours;
+
+	cout << "Pay per hour: ";
+	cin >> hourly_rate;
 
 	//Create a double variable named gross_salary
 	//Call the gross_pay function and save its return value to the gross_salary variable
+	double gross_salary;
+	gross_salary = gross_pay(hourly_rate, hours);
 
 	//Display the gross_salary variable as follows:
 	//Example ---->>>     Gross pay: 150.00   
+	cout << "Total pay is: " << gross_salary;
 
 	return 0;
 }
