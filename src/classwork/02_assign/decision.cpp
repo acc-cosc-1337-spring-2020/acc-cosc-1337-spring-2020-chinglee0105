d87e7d7c895f5ea798b01cd6e0eb294682ed0e08
Art Gonzalez <arturo.gonzalez@austincc.edu>
2020-02-06 22:28:28 -0600

    classwork 2 solution: ot pays 40 regular hours

--- src/classwork/02_assign/decision.cpp
+++ src/classwork/02_assign/decision.cpp
@@ -4,14 +4,14 @@
 double gross_pay(double hours, double hourly_rate) {
 	double pay;
 
-	if (hours < 40) {
+	if (hours <= 40) {
 		pay = hours * hourly_rate;
 
 		return pay;
 	}
 
 		else if (hours > 40) {
-			pay = (hours * hourly_rate) + ((hours - 40) * (hourly_rate * 1.5));
+			pay = (40 * hourly_rate) + ((hours - 40) * (hourly_rate * 1.5));
 
 		return pay;
 	}
