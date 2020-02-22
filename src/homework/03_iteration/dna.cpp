96655f4f766a1a2c6cc2b4bd2199043269fa51bb
Ching Lee <ching.lee@g.austincc.edu>
2020-02-21 23:04:53 -0600

    homework 3 complete. NOTE: instruction for TravisCI was wrong "- ./test/homework_test/03_iteration/hw_03_tests -s", it suppose to be "- ./test/homework_test/03_iteration_test/hw_03_tests -s". also, instruction of CMakeLists is confusing.

--- src/homework/03_iteration/dna.cpp
+++ src/homework/03_iteration/dna.cpp
@@ -1,4 +1,9 @@
 #include "dna.h"
+#include <string>
+using std::string;
+
+using namespace std;
+
 /*
 Write code for function get_gc_content that accepts
 a const reference string parameter and returns a double.
@@ -6,7 +11,18 @@ Calculate GC content:
 Iterate string count Gs and Cs, divide count by string length.
 Return quotient.
 */
+double get_gc_content(const string& dna) {
+	double count = 0;
 
+	for (int i = 0; i < dna.length(); ++i) {
+
+		if (dna[i] =='G' || dna[i] == 'C') {
+			++count;
+		}
+	}
+
+	return double(count / dna.length());
+}
 
 
 
@@ -14,6 +30,12 @@ Return quotient.
 Write code for function get_reverse_string that
 accepts a string parameter and returns a string reversed.
 */
+string get_reverse_string(string dna) {
+ 
+	reverse(dna.begin(), dna.end());
+
+	return dna;
+}
 
 
 
@@ -25,6 +47,28 @@ a. call function get_reverse_string(dna), save results to a local string variabl
 b. iterate local string variable and
     replace A with T, T with A, C with G and G with C
 c. return string
-
 */
+string get_dna_complement(string dna) {
+	string reversed = get_reverse_string(dna);
 
+	for (int i = 0; i < reversed.length(); ++i) {
+
+		if (reversed[i] == 'A') {
+			reversed[i] = 'T';
+		}
+
+		else if (reversed[i] == 'T') {
+			reversed[i] = 'A';
+		}
+
+		else if (reversed[i] == 'C') {
+			reversed[i] = 'G';
+		}
+
+		else if (reversed[i] == 'G') {
+			reversed[i] = 'C';
+		}
+	}
+
+	return reversed;
+}
--- src/homework/03_iteration/main.cpp
+++ src/homework/03_iteration/main.cpp
@@ -1,6 +1,12 @@
 //write include statements
+#include<iostream>
+#include<string>
+#include "dna.h"
 
 //write using statements
+using std::cout;
+using std::cin;
+using std::string;
 
 
 /*
@@ -10,7 +16,48 @@ DNA string and call either get gc content or get dna complement
 function and display the result. Program runs as long as 
 user enters a y or Y.
 */
-int main() 
-{
-	return 0;
+int main() {
+	int choice;
+
+	string dna;
+	string result;
+	string decisionToProceed;
+
+	bool proceed = true;
+	bool errorOccured = false;
+
+	while (proceed) {
+
+		if (errorOccured == false) {
+			cout << "Enter 1 for Get GC Content or 2 for Get DNA Complement: ";
+			cin >> choice;
+		}
+
+		cout << "Enter your DNA: ";
+		cin >> dna;
+
+		if (choice == 1) {
+			cout << get_gc_content(dna);
+		}
+
+		else if (choice == 2) {
+			cout << get_dna_complement(dna);
+		}
+
+		else {
+			cout << "Error! Please enter 1 for Get GC Content or 2 for Get DNA Complement: ";
+			cin >> choice;
+			errorOccured = true;
+			continue;
+		}
+
+		cout << "Do you wish to continue? (Y/y): ";
+		cin >> decisionToProceed;
+
+		if (decisionToProceed != "y" && decisionToProceed != "Y") {
+			proceed = false;
+		}
+	}
+
+	cout << "End of program.";
 }
